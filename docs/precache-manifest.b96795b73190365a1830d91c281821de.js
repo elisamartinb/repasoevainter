d19ede4e017850907407157deae7a38a9a2df38d
elisamartinb <elisamartinbt@gmail.com>
2019-01-13 23:56:41 +0100

    build

---
+++ docs/precache-manifest.b96795b73190365a1830d91c281821de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "442dd4152e94ddfacd1b",
+    "url": "./static/js/main.442dd415.chunk.js"
+  },
+  {
+    "revision": "13ef3da1ec417616d67e",
+    "url": "./static/js/1.13ef3da1.chunk.js"
+  },
+  {
+    "revision": "442dd4152e94ddfacd1b",
+    "url": "./static/css/main.191616fb.chunk.css"
+  },
+  {
+    "revision": "900bc3d4889e297a01adb52dd8549fd8",
+    "url": "./index.html"
+  }
+];
